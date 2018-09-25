091b61b2e10c4d2484e47d563fd9a376d30197c8
Onegodless <onegodlesssoab@gmail.com>
2018-09-25 14:58:27 +0200

    multiple result to search added

--- server_applications/search_application.php
+++ server_applications/search_application.php
@@ -1,12 +1,12 @@
-<?php
-
+<?php 
 $db = new mysqli('localhost', 'root', '1234', 'mrrobot');
 if (!$db){
 	echo 'Cannot connect to db';
 };
-
+$like = '%';
 $user_search = $_REQUEST["user_search"];
-$query = "SELECT * FROM ps4_games WHERE name = ?";
+$user_search = $user_search . '%';
+$query = "SELECT * FROM ps4_games WHERE name LIKE ?";
 $stmt = $db->prepare($query);
 $stmt->bind_param('s', $user_search);
 $stmt->execute();
@@ -16,7 +16,7 @@ $reply="";
 
 $stmt->bind_result($id, $name, $release_date, $genre, $cover_img);
 while ($stmt->fetch()){
-	$reply =  "$name";
-};
+	$reply =  $reply . "<p>$name</p><p>$release_date</p><p>$genre</p><img src='$cover_img'>";
+}
 echo $reply;
 ?>
