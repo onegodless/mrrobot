21edcecbc53966e13bb94f5a212c56aef9f5d04d
Onegodless <onegodlesssoab@gmail.com>
2018-09-30 01:54:27 +0200

    JSON added, javascript renders search results now

--- script/form_validate.js
+++ script/form_validate.js
@@ -12,13 +12,48 @@ function createXmlHttpObject(){
 function handleRequestStateChange(){
 	
 	if(xmlHtmlObject.readyState == 4 && xmlHtmlObject.status == 200){
-		document.getElementById("search_results").innerHTML = xmlHtmlObject.responseText;
+		var response = JSON.parse(xmlHtmlObject.responseText);
+		response.forEach(function(element){
+
+			var new_article = document.createElement("article");
+			var article_att = document.createAttribute("class");
+			article_att.value = "game_box";
+			new_article.setAttributeNode(article_att);
+
+			var new_img = document.createElement("img");
+			var img_att = document.createAttribute("src");
+			var img_att_clss = document.createAttribute("class");
+			img_att.value = element.cover_img_link;
+			img_att_clss.value = "cover_img";
+			new_img.setAttributeNode(img_att);
+			new_img.setAttributeNode(img_att_clss);
+
+			var new_section = document.createElement("section");
+			var section_att = document.createAttribute("class");
+			section_att.value = "game_info";
+			new_section.setAttributeNode(section_att);
+			
+			var new_name = document.createElement("p");
+			new_name.innerHTML = element.name;
+			new_section.appendChild(new_name);
+
+			var new_price = document.createElement("p");
+			new_price.innerHTML = element.price;
+			new_section.appendChild(new_price);
+
+
+			new_article.appendChild(new_img);
+			new_article.appendChild(new_section);
+			document.getElementById("search_results").appendChild(new_article);
+
+		});
 	}
 }
 
 
 function check_form(){
 
+	document.getElementById("search_results").innerHTML = "";//wipes the results section before adding any new result.
 	var search_field = document.getElementById("user_search").value;
 	var trim_search_field = search_field.trim();
 	trim_search_field = trim_search_field.toLowerCase();
--- srv_apps/search_app.php
+++ srv_apps/search_app.php
@@ -1,23 +1,19 @@
 <?php 
-$db = new mysqli('localhost', 'root', '1234', 'mrrobot');
-if (!$db){
+$mysqli = new mysqli('localhost', 'root', '1234', 'mrrobot');
+if (!$mysqli){
 	echo 'Cannot connect to db';
 };
 
 $user_search = $_GET["user_search"];
 $user_search = $user_search . '%';
 
-$query = "SELECT * FROM ps4_games WHERE name_db LIKE ?";
-$stmt = $db->prepare($query);
-$stmt->bind_param('s', $user_search);
-$stmt->execute();
-$stmt->store_result();
+$sql = "SELECT name, cover_img_link FROM ps4_games WHERE name_db LIKE '$user_search'" ;
+$query = $mysqli->query($sql);
 
-$reply="";
-
-$stmt->bind_result($id, $name_db, $name, $release_date, $genre, $cover_img, $price);
-while ($stmt->fetch()){
-	$reply =  $reply . '<article id="game_box">' . '<img id="cover_img" src=' . $cover_img . '> <section id="game_info"> <p>' . $name . '</p><p>' . '<p>' . $price . '€</p>'. $release_date . '</p> <p>' . $genre . '</p>' . '</section></article>';
+while ( $row = $query->fetch_assoc() ){
+	$reply[] = $row;
 }
-echo $reply;
+$query->free_result();
+
+echo json_encode($reply);
 ?>
