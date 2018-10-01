53d412f74428004e0467368ab332e162873332b0
Onegodless <onegodlesssoab@gmail.com>
2018-10-02 01:39:40 +0200

    buildin singup, program checks username aviability

--- script/validate_singup.js
+++ script/validate_singup.js
@@ -1,20 +1,57 @@
 var xhtml = new XMLHttpRequest();
 
+var user_name_taken = false;
+var user_name_syntax = false;
+
+
+function validateUserName(user_name_taken){
+
+	var advise_box = document.getElementById("advise_box");
+	var user_name_taken = user_name_taken;
+
+	advise_box.innerHTML = "";//resets the box.
+	console.log(name);
+	if(name.includes(" ")){
+		var new_paragraph = document.createElement("p");
+		new_paragraph.innerHTML = "User name cannot contain spaces.";
+		advise_box.appendChild(new_paragraph);
+	}else{
+		user_name_syntax = true;
+
+	}
+
+	if(user_name_taken && user_name_syntax){
+		console.log("succes");
+		user_name_not_take = false;
+		user_name_syntax = false;
+	}
+
+}
+
+
 function readyFunction(){
-	if (xhtml.readyState == 2 && xhtml.status == 400){
+
+	if (xhtml.readyState == 4 && xhtml.status == 200){
 		var response = xhtml.responseText;
-		console.log("hello world!");
-		document.getElementById("answer").InnerHTML = response;
+		document.getElementById("answer").innerHTML = response;
+		if(response == "The user name is aviable"){
+			user_name_taken = true;
+			validateUserName(user_name_taken);
+		}
 	}
 }
 
-function validate(){
+
+function main(){
+
+	var name = document.getElementById("user_name").value;
+	var pass = document.getElementById("user_psswd").value;
+
 	if(xhtml){
-		xhtml.open("POST", "srv_apps/check_singup.php", true);
+
+		xhtml.open("GET", "srv_apps/check_singup.php?user_name=" + name, true);
 		xhtml.onreadystatechange = readyFunction;
-		var name = document.getElementById("user_name").value;
-		console.log(name);
-		xhtml.send("user_name=" + name);
-		
+		xhtml.send();
 	}
+
 }
--- srv_apps/check_singup.php
+++ srv_apps/check_singup.php
@@ -1,3 +1,14 @@
 <?php
-	echo $_POST["user_name"];
+$user_name = $_GET["user_name"];
+
+$mysqli = new mysqli('localhost','mrrobot','1234','mrrobot');
+
+$sql = "SELECT user_name FROM users WHERE user_name = '$user_name'";
+$query = $mysqli->query($sql);
+$num_results = $query->num_rows;
+if($num_results > 0){
+	echo "This user name is already used";
+}elseif($num_results == 0){
+	echo "The user name is aviable";
+}
 ?>
